9526c3f8f646b6ad7cfacfd040643afdeb05ab7b
Kok How, Teh <funcoolgeek@gmail.com>
2025-04-04 10:48:31 +0800

    Remove export

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-export USER="${NEO4J_AUTH%/*}"
-export PASSWORD="${NEO4J_AUTH#*/}"
+USER="${NEO4J_AUTH%/*}"
+PASSWORD="${NEO4J_AUTH#*/}"
 cp labs/apoc-2025.02.0-core.jar plugins/
 bin/neo4j-admin dbms set-initial-password $PASSWORD || true
 #neo4j-admin database load neo4j --from-path=/import
