9d6cfce641f6fc7ca0f8be5144f7dbf821fc97a7
Kok How, Teh <funcoolgeek@gmail.com>
2025-12-23 16:40:55 +0800

    Replace obsolete MAINTAINER with LABEL

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 FROM neo4j:latest
+LABEL org.opencontainers.image.authors="Kok How, Teh <funcoolgeeek@gmail.com>"
 VOLUME /data
 ADD run.sh /usr/local/bin/run.sh
 ENTRYPOINT ["/usr/local/bin/run.sh"]
--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #docker build -t khteh/neo4j . --build-arg NEO4J_AUTH='neo4j/P@$$w0rd'
-docker build -t khteh/neo4j .
+docker build -t khteh/neo4j . --no-cache
 docker push khteh/neo4j:latest
